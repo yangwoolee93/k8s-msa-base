e682a2815ef27978d627600f83f72f7c10791819
yangwoo2 <yangwoolee93@gmail.com>
2026-07-10 11:42:18 +0900

    feat: user-auth 서비스 초기 구축
    health체크만 있는 상태의 api 서비스

--- apps/user-auth/src/main.ts
+++ apps/user-auth/src/main.ts
@@ -1,20 +1,20 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const swaggerConfig = new DocumentBuilder()
-    .setTitle('api-gateway')
-    .setDescription('API Gateway — health and proxy entrypoint')
+    .setTitle('user-auth')
+    .setDescription('User Auth Service')
     .setVersion('0.1.0')
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, document);
 
-  const port = process.env.API_GATEWAY_PORT || 4000;
+  const port = process.env.USER_AUTH_PORT || 4001;
   await app.listen(port);
   Logger.log(`Application is running on: http://localhost:${port}`);
   Logger.log(`Swagger docs: http://localhost:${port}/docs`);
